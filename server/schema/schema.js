da6ac01a457a5e30118cc3b876d91593585fee02
Rahul Desai <rd825@georgetown.edu>
2018-11-13 10:09:18 -0500

    Installed mongoose and connected to mLab

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,10 +2,11 @@
 const _ = require('lodash');
 
 const graphql = require('graphql');
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
+    // fields get wrapped in function, bc if they execute immediately you get a catch-22 type error (bc the different types refer to each other)
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
@@ -24,7 +25,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent,args) {
+                return _.filter(books, {authorId: parent.id})
+            }
+        }
     })
 });
 
@@ -32,7 +39,10 @@ const AuthorType = new GraphQLObjectType({
 let books = [
     {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
     {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'}
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
 ]
 
 let authors = [
@@ -57,6 +67,12 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLID}},
             resolve(parent,args){
                 return _.find(authors, {id: args.id})
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parents,args){
+                return books;
             }
         }
     }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
 const graphqlHTTP = require('express-graphql'); // the const name is a graphQL convention
 // we use it as middleware on a single route (our supercharged single endpoint)
 
-const schema = require('./schema/schema');
-
 const server = express();
+
+mongoose.connect('mongodb://rahul:test123@ds037698.mlab.com:37698/gql-rahul')
+mongoose.connection.once('open', () => console.log('Connected to DB'))
+
 server.use('/graphql', graphqlHTTP({
     // the schema goes in here
     schema,
