36f009aec1440ce3817a28c9801fe417aebd9a5d
Rahul Desai <rd825@georgetown.edu>
2018-11-13 09:02:14 -0500

    Set up basic graphql schema

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const server = express();
-const port = process.env.PORT || 9000;
+const graphqlHTTP = require('express-graphql'); // the const name is a graphQL convention
+// we use it as middleware on a single route (our supercharged single endpoint)
 
+const schema = require('./schema/schema');
+
+const server = express();
+server.use('/graphql', graphqlHTTP({
+    // the schema goes in here
+    schema,
+    graphiql: true
+})); 
+
+const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`Listening on port ${port}`))
