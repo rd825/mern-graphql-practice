9bd80db15cfed12e6ab4b884e4c43c7c598d6502
Rahul Desai <rd825@georgetown.edu>
2018-11-13 18:39:55 -0500

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 const graphqlHTTP = require('express-graphql'); // the const name is a graphQL convention
 // we use it as middleware on a single route (our supercharged single endpoint)
 
 const server = express();
+server.use(cors());
 
 mongoose.connect('mongodb://rahul:test123@ds037698.mlab.com:37698/gql-rahul')
 mongoose.connection.once('open', () => console.log('Connected to DB'))
