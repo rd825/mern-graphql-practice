b55a345058d3331fddbaab9dab13a15f36988b36
Rahul Desai <rd825@georgetown.edu>
2018-11-13 18:59:30 -0500

    Working on using GraphQL in the front-end with Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import BookList from './components/BookList';
 
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo'
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:9000/graphql'
+})
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Reading List</h1>
-        <BookList />
-      </div>
+        <ApolloProvider client={client}>
+          <div id="main">
+
+            <h1>Reading List</h1>
+            <BookList />
+          
+          </div>
+        </ApolloProvider>
     );
   }
 }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
 
 class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-            <li>Book</li>
-        </ul>
-      </div>
-    );
-  }
+
+    displayBooks() {
+        let data = this.props.data;
+        if (data.loading) {
+            return(<div>Loading books...</div>)
+        } else {
+            return data.books.map(book => {
+                return(
+                    <li>{book.name}</li>
+                )
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ul id='book-list'>
+                    <li>Book name</li>
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
